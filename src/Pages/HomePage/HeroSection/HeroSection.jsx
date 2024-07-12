add0e4ffad116671f770df9323c279e1e389a94f
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-07-12 22:57:21 +0600

    change hero section

--- src/Pages/HomePage/HeroSection/HeroSection.jsx
+++ src/Pages/HomePage/HeroSection/HeroSection.jsx
@@ -1,39 +1,30 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import { FlipWords } from "../../../components/FlipWords/FlipWords";
-
+import image from "./undraw_survey_05s5.svg";
 const HeroSection = () => {
-  const words = [
-    "Engage",
-    "Discover",
-    "Analyze",
-    "Succeed",
-    "Insightful",
-    "Innovative",
-    "Capture",
-    "Feedback",
-    "Analyze",
-    "Reliable",
-    "Decide",
-    "Customizable",
-  ];
-
   return (
-    <div className="h-screen items-center justify-center flex">
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 py-4 bg-clip-text text-center text-5xl font-medium tracking-tight md:text-7xl"
-      >
-        <Link to="/surveys">
-          <FlipWords words={words} /> <br /> <span>With Survey Genius</span>
-        </Link>
-      </motion.h1>
+    <div>
+      <div className=" lg:flex mt-20 justify-center items-center py-10 gap-10 p-4">
+        <div className="lg:mb-0 mb-4">
+          <p className="text-sm md:text-xl font-sans">
+            Faster, Smarter, Affofdable Survey Software
+          </p>
+          <h1 className="text-2xl lg:text-5xl tracking-wide font-serif font-semibold my-1">
+            Powerful online survey
+          </h1>
+          <p>
+            Experience perfect combination of speed to value, ease of use, and
+            security.
+          </p>
+          <button className="my-4">
+            <Link className=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+              Explore
+            </Link>
+          </button>
+        </div>
+        <div>
+          <img src={image} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
